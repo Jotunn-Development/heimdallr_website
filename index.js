859a83be8a04183f26c22be8bd64a3b8cd284756
VulgarBear <lucas@shisify.com>
2020-12-05 18:45:37 -0500

    refactor: :construction: Moved dashboard site from bot to external page.
    The dashboard site from now on out will be externally hosted from the bot. This is to allow changes to be made without effecting the bot. The bot will instead be running an API in the future.
    
    Disable dashboard on bot.

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+const path = require('path')
+const signale = require('signale')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// express settings
+app
+  .use(express.static(path.join(__dirname, './')))
+  .engine('html', require('ejs').renderFile)
+  .set('view engine', 'ejs')
+  .set('views', path.join(__dirname, 'views'))
+
+// get pages
+app
+  .get('/', (req, res) => res.render('pages/index.ejs'))
+
+// start server
+app.listen(port, () => { signale.success({prefix: '[Express]', message: `Website has been established on ${port}`}) })
